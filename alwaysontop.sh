a1b3d69ad76b323223731be8503ddf0209f04844
Peter Swire <swirepe@gmail.com>
2013-06-26 13:49:39 -0400

    initial commit

---
+++ alwaysontop.sh
@@ -0,0 +1,204 @@
+export PS1
+export PROMPT_COMMAND
+
+# the colors used here
+COLOR_off='\033[0m' 
+COLOR_BIPurple='\033[1;95m' 
+COLOR_BIYellow='\033[1;93m'
+COLOR_IBlack='\033[0;90m'
+COLOR_BGreen='\033[1;32m'
+COLOR_BRed='\033[1;31m'
+
+
+## the custom indicators
+export ALWAYSONTOP_INDICATOR="\[${COLOR_BIPurple}\]↑↑\[${COLOR_off}\] "
+export AUTOCLEAR_INDICATOR="\[${COLOR_BIYellow}\]◎\[${COLOR_off}\] "
+
+#export ALWAYSONTOP_INDICATOR="^^ "
+#export AUTOCLEAR_INDICATOR="@@ "
+
+
+
+function alwaysontop {
+    if [ "$ALWAYSONTOP" != "TRUE" ]
+    then
+        export ALWAYSONTOP="TRUE"
+        export OLD_PROMPT_COMMAND_AOT="$PROMPT_COMMAND"
+        if [ "$PROMPT_COMMAND" ]
+        then
+            # before showing the prompt and doing whatever you're supposed to do
+            # when you do that, go to the top of the screen and clear in both directions
+            PROMPT_COMMAND="$PROMPT_COMMAND ; tput cup 0 0 ; tput el ; tput el1"
+        else
+            PROMPT_COMMAND="tput cup 0 0 ; tput el ; tput el1"
+        fi
+        
+        
+        PS1="$ALWAYSONTOP_INDICATOR$PS1"
+        #PS1="$PS1"
+    fi
+    
+    echo -e "[alwaysontop.sh] ${COLOR_BIPurple}always on top${COLOR_off} ${COLOR_BGreen}ON${COLOR_off}."
+}
+
+
+function unalwaysontop {
+    if [ "$ALWAYSONTOP" == "TRUE"  ]
+    then
+        if [ -n $OLD_PRMOPT_COMMAND_AOT ]
+        then
+            PROMPT_COMMAND="$OLD_PRMOPT_COMMAND_AOT"
+        fi
+        
+        ALWAYSONTOP="FALSE"
+        
+        # The ${string##substring} is bash for "remove the longest substring that matches"
+        PS1=`perl -e '$newps1 = $ENV{"PS1"}; $newps1 =~ s/\Q$ENV{"ALWAYSONTOP_INDICATOR"}//; print $newps1'`
+    fi
+   
+        echo -e "[alwaysontop.sh] ${COLOR_BIPurple}always on top${COLOR_off} ${COLOR_BRed}OFF${COLOR_off}."
+}
+
+
+function autoclear {
+    if [ "$AUTOCLEAR" != "TRUE" ]
+    then
+        export AUTOCLEAR="TRUE"
+        
+        # replace the enter key with a form feed (clears the screen) and an enter key
+        bind 'RETURN: "\C-l\C-j"' 
+        PS1="$AUTOCLEAR_INDICATOR$PS1"
+        
+        #PS1="$PS1"
+    fi
+    
+    # since we are going to be clearing the screen after every command, might as well have cd also be an ls
+    alias "cd"=cdls
+    
+    # all those little navigation functions that basically just cd into a directory?
+    # let them know to use the new cd function
+    # i'm thinking, for example, of whatever magic rvm uses
+    renavigate    
+    
+    echo -e "[alwaysontop.sh] ${COLOR_BIYellow}autoclear${COLOR_off} ${COLOR_BGreen}ON${COLOR_off}."
+}
+
+
+function unautoclear {
+    export AUTOCLEAR="FALSE"
+    bind 'RETURN: "\C-j"'
+    PS1=`perl -e '$newps1 = $ENV{"PS1"}; $newps1 =~ s/\Q$ENV{"AUTOCLEAR_INDICATOR"}//; print $newps1'`
+    
+    unalias "cd"
+    renavigate
+    
+    echo -e "[alwaysontop.sh] ${COLOR_BIYellow}autoclear${COLOR_off} ${COLOR_BRed}OFF${COLOR_off}."
+}
+
+
+# turn on both alwaysontop and autoclear
+function autotop {
+    clear
+    autoclear
+    alwaysontop
+}
+
+
+# turn off both alwaysontop and autoclear
+function unautotop {
+    unalwaysontop
+    unautoclear
+}
+
+
+function hr {
+    echo -en "${COLOR_IBlack}"
+    eval printf '%.0s-' {1..$(tput cols)}
+    echo -en $COLOR_off 
+}
+
+
+function cdls {
+    # go into a directory
+    # if that succees, print the git status and a horizontal rule (if we are in a git repository)
+    # then print the directory contents
+    
+    # other things you might want to do: 
+    #    force ls to use color, put it into columns, pipe that into head -n$LINES
+    # the BSD (read: Mac OSX) version of ls is different from the one I use at home, 
+    # so I am just leaving this as ls
+    command cd "$@" && ((git status -bs 2>/dev/null && hr) ; ls  )
+}
+
+
+function renavigate {
+    ## reloads my navigation functions so that they get the new cd alias
+    ## or do nothing
+    # source $BASHINCLUDES_DIR/navigation.sh
+    echo "[autotop.sh] renavigate not implemented." > /dev/stderr
+}
+
+
+function alwaysontop_help {
+    echo -e "alwaysontop.sh - keep the prompt at the top of the screen."
+    echo -e "Included commands:"
+    echo -e "    "
+    echo -e "    alwaysontop_help  This screen"
+    echo -e "    "
+    echo -e "    autotop           Turn ${COLOR_BGreen}ON${COLOR_off} always on top and autoclear"
+    echo -e "    unautotop         Turn ${COLOR_BRed}OFF${COLOR_off} always on top and autoclear"
+    echo -e "    "                 
+    echo -e "    alwaysontop       Turn ${COLOR_BGreen}ON${COLOR_off} always on top"
+    echo -e "    unalwaysontop     Turn ${COLOR_BRed}OFF${COLOR_off} always on top"
+    echo -e "    "                 
+    echo -e "    autoclear         Turn ${COLOR_BGreen}ON${COLOR_off} clear-screen after each command."
+    echo -e "    unautoclear       Turn ${COLOR_BRed}OFF${COLOR_off} clear-screen after each command."
+    echo -e "    "
+    echo -e "    alwaysontop indicator:  ${COLOR_BIPurple}↑↑${COLOR_off}"
+    echo -e "    autoclear indicator:    ${COLOR_BIYellow}◎${COLOR_off}"
+
+}
+
+
+
+if [[ "$BASH_SOURCE" == "$0" ]]
+then
+    echo "[alwaysontop.sh] You should source this file." > /dev/stderr
+    exit 1
+else
+
+    autotop
+    echo "    "  
+    alwaysontop_help
+fi
+
+
+
+
+#   case "$1" in
+#       -h|--help|help)
+#           alwaysontop_help
+#           shift
+#           exit 0
+#           ;;
+#       -a|autotop|--autotop)
+#           shift
+#           autotop
+#           ;;
+#       --alwaysontop)
+#           alwaysontop
+#           shift
+#           ;;
+#       --autoclear)
+#           autoclear
+#           shift
+#           ;;
+#       *)
+#           alwaysontop_help
+#           echo "    "  
+#           autotop
+#           ;;
+#   esac
+
+
+
